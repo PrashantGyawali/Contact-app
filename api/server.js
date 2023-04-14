0c2d72dda13e00f8881b74792e4f3fb9e15b9934
pes18fan <pes18fan@disr.it>
2023-04-14 21:46:19 +0545

    [fix] attempt 7: fix deployment

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const express = require("express");
 const path = require("path");
+const { json } = require('body-parser');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -8,6 +9,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3006;
 
 server.use("/contacts", middlewares, router);
+
 server.use(express.static(path.join(__dirname, "build")));
 server.get("/*", (_req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
