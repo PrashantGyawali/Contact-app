cc5daa2e4b2fb34b98448cbc0f4d6900a1c1b89e
pes18fan <pes18fan@disr.it>
2023-04-15 11:18:11 +0545

    [partial_revert] ae3a1ae: change baseURL back to /

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 axios.defaults.port = process.env.SERVER_PORT || 3006;
 export default axios.create(
     {
-        baseURL: `http://localhost:${axios.defaults.port}/`
+        baseURL: "/"
     }
 )
